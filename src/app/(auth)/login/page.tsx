fa1ca198626e58601f63b67f4236a32c1871c665
raji <kingraj1344@gmail.com>
2024-10-05 14:29:32 +0100

    fixed

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,12 +2,12 @@ import { FormWrapper } from "@/components/formWrapper";
 import { LoginForm } from "@/components/forms"; // LoginForm is in forms
 
 
-export default function Page({searchParams}:{searchParams:{email:string,password:string,}}) {
-   const defaultValues = searchParams
+export default function Page() {
+
 
   return (
     <FormWrapper hideProgressBar={true} login={true}> {/* Hide progress bar and set login mode */}
-      <LoginForm defaultValues={defaultValues} />
+      <LoginForm  />
       
     </FormWrapper>
   );
