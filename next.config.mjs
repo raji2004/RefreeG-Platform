1860cd0e5d8807d2cfea169f8cb6b9c109e48455
Nomso <nomsounaogu@gmail.com>
2024-10-08 08:42:48 +0100

    revert next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // time in seconds of no pages generating during static generation before timing out
-  staticPageGenerationTimeout: 1000,
-};
+const nextConfig = {};
 
 export default nextConfig;
