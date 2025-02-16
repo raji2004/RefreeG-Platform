c3b4bcea46e8a64e6d6659908260749052c1d195
Nomso <nomsounaogu@gmail.com>
2025-02-16 14:45:38 +0100

    successfully created UI for crypto feature

--- src/app/crypto-donation/page.tsx
+++ src/app/crypto-donation/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import { getContract } from "thirdweb";
+import { client } from "./client";
+import { polygonAmoy } from "thirdweb/chains";
+import { CROWDFUNDING_FACTORY } from "../../constants/contracts";
+import { useReadContract } from "thirdweb/react";
+import { CampaignCard } from "../../components/CampaignCard";
+
+export default function Home() {
+  const contract = getContract({
+    client: client,
+    chain: polygonAmoy,
+    address: CROWDFUNDING_FACTORY,
+  });
+  // Get all campaigns deployed with CrowdfundingFactory
+  const {
+    data: campaigns,
+    isLoading: isLoadingCampaigns,
+    refetch: refetchCampaigns,
+  } = useReadContract({
+    contract: contract,
+    method:
+      "function getAllCampaigns() view returns ((address campaignAddress, address owner, string name)[])",
+    params: [],
+  });
+
   return (
-    <div>page</div>
-  )
+    <main className="mx-auto max-w-7xl px-4 mt-4 sm:px-6 lg:px-8">
+      <div className="py-10">
+        <h1 className="text-4xl font-bold mb-4">Campaigns:</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {!isLoadingCampaigns &&
+            campaigns &&
+            (campaigns.length > 0 ? (
+              campaigns.map((campaign) => (
+                <CampaignCard
+                  key={campaign.campaignAddress}
+                  campaignAddress={campaign.campaignAddress}
+                />
+              ))
+            ) : (
+              <p>No Campaigns</p>
+            ))}
+        </div>
+      </div>
+    </main>
+  );
 }
-
-export default page
