238745ced57cd52b3b440a137e2ed063f2a0b224
Hassan Quadri <hastelbee06@gmail.com>
2025-02-17 19:18:24 +0100

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        type: "asset/resource",
+      });
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
