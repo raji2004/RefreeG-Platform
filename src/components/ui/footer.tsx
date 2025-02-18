38ea42fd53063f840c98b9e60e3405f4c95283b4
Hassan Quadri <hastelbee06@gmail.com>
2025-02-18 13:11:01 +0100

    home page restructuring

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -24,8 +24,8 @@ export function Footer() {
             <footer className="bg-blue-50">
 
                 <div className="lg:flex ml-10 mr-10 pt-8 border-b border-gray-500">
-                    <div className="md:flex md:justify-between lg:justify-between lg:w-6/12 ">
-                        <div className="flex flex-col w-full mb-16 md:w-1/2 lg:w-8/12 md:mr-16">
+                    <div className="md:flex md:justify-between lg:justify-between lg:w-7/12 ">
+                        <div className="flex flex-col w-full mb-16 md:w-1/2 lg:w-7/12 md:mr-16">
                             <div className="text-blue-950 text-2xl font-semibold pb-3">Subscribe</div>
                             <div className="text-lg pb-3">Join our newsletter to stay up to date on features and releases.</div>
                             <form className="flex justify-between bg-white px-3 py-3 mb-3 w-full rounded-full">
@@ -35,21 +35,21 @@ export function Footer() {
                             </form>
                             <div className="text-lg">By Subscribing you agree with our <Link href='#' className="underline text-blue-950 font-semibold">Privacy policy</Link></div>
                         </div>
-                        <div className="flex flex-col w-full md:w-1/2 md:pl-16">
+                        <div className="flex flex-col w-full md:w-1/2 lg:w-5/12 md:pl-16">
                             <div className="text-blue-950 text-2xl pb-3 font-semibold">Quick Links</div>
                             <Link href="#" className="underline text-lg mb-3">Home</Link>
                             <Link href="#" className="underline text-lg mb-3">About Us</Link>
                             <Link href="#" className="underline text-lg mb-3">Who we are</Link>
                         </div>
                     </div>
-                    <div className="md:flex md:justify-between lg:justify-between lg:w-6/12">
+                    <div className="md:flex md:justify-between lg:justify-between lg:w-5/12">
                         <div className="flex flex-col w-full mb-12 md:w-1/2">
                             <div className="text-blue-950 text-2xl pb-3 font-semibold">Contact Us</div>
                             <Link href="#" className="underline hidden md:text-lg md:block mb-3">refreegcorrespondence@gmail.com</Link>
                             <Link href="#" className="underline text-lg mb-3">Phone: +234-090-174-6760</Link>
                             <Link href="#" className="underline text-lg mb-3">Abuja, Nigeria</Link>
                         </div>
-                        <div className="flex flex-col w-full md:w-2/5">
+                        <div className="flex flex-col w-full md:w-2/5 lg:w-2/12">
                             <div className="text-blue-950 text-2xl pb-3 font-semibold">Legal</div>
                             <Link href='#' className="underline text-lg mb-3">Privacy Policy</Link>
                             <Link href='#' className="underline text-lg mb-3">Terms of Service</Link>
