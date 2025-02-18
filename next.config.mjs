ac4b928e4056444b6542cbf06765cfca41d2b2d5
raji <kingraj1344@gmail.com>
2025-02-18 18:01:26 +0100

    Comment out webpack configuration for asset resource handling

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        type: "asset/resource",
-      });
-      return config;
-    },
+    // webpack: (config) => {
+    //   config.module.rules.push({
+    //     test: /\.(woff|woff2|eot|ttf|svg)$/,
+    //     type: "asset/resource",
+    //   });
+    //   return config;
+    // },
   };
   
   export default nextConfig;
