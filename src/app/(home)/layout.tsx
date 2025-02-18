5e16c36657c264fe0dd09a0c38e65691a04ffa60
Hassan Quadri <hastelbee06@gmail.com>
2025-02-18 12:41:11 +0100

    footer update

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,10 +4,13 @@ import { Footer } from "@/components/ui/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="mr-10 ml-10 mb-16">
+        <div>
+            <div className="mr-10 ml-10 mb-16">
 
-            <Navbar />
-            {children}
+                <Navbar />
+                {children}
+                
+            </div>
             <Footer />
         </div>
     )
