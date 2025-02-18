67f0a0134f0f85bf07d5283f2e277369d5918459
Hassan Quadri <hastelbee06@gmail.com>
2025-02-18 13:07:43 +0100

    home page restructuring

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,14 +4,14 @@ import { Footer } from "@/components/ui/footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
-            <div className="mr-10 ml-10 mb-16">
+        <>
+            <div className=" mb-16">
 
                 <Navbar />
                 {children}
                 
             </div>
             <Footer />
-        </div>
+        </>
     )
 }
