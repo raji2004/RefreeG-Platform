197692ac091fe1507fda3c9fbf1d796798ed8221
Hassan Quadri <hastelbee06@gmail.com>
2025-02-18 20:15:59 +0100

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        type: "asset/resource",
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -43,13 +43,13 @@ export function Footer() {
                         </div>
                     </div>
                     <div className="md:flex md:justify-between lg:justify-between lg:w-5/12">
-                        <div className="flex flex-col w-full mb-12 md:w-1/2">
+                        <div className="flex flex-col w-full mb-12 md:w-1/2 lg:w-6/12">
                             <div className="text-blue-950 text-2xl pb-3 font-semibold">Contact Us</div>
                             <Link href="#" className="underline hidden md:text-lg md:block mb-3">refreegcorrespondence@gmail.com</Link>
                             <Link href="#" className="underline text-lg mb-3">Phone: +234-090-174-6760</Link>
                             <Link href="#" className="underline text-lg mb-3">Abuja, Nigeria</Link>
                         </div>
-                        <div className="flex flex-col w-full md:w-2/5 lg:w-2/12">
+                        <div className="flex flex-col w-full md:w-2/5 lg:w-4/12">
                             <div className="text-blue-950 text-2xl pb-3 font-semibold">Legal</div>
                             <Link href='#' className="underline text-lg mb-3">Privacy Policy</Link>
                             <Link href='#' className="underline text-lg mb-3">Terms of Service</Link>
