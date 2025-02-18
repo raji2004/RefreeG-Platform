cd712ecef76f1b08dd78d59e68aa8fc8ffa0f542
raji <kingraj1344@gmail.com>
2025-02-18 17:32:57 +0100

    searching for error

--- src/app/UserFlow/SignedPetitions/page.tsx
+++ src/app/UserFlow/SignedPetitions/page.tsx
@@ -3,20 +3,20 @@
 import React, { useState } from 'react'
 
 
-import Sidebar from "@/components/Sidebar";
-import Topbar from "@/components/Topbar";
-import SignedPetitions from '@/components/UserProfileComponents/signedPetitions';
+// import Sidebar from "@/components/Sidebar";
+// import Topbar from "@/components/Topbar";
+// import SignedPetitions from '@/components/UserProfileComponents/signedPetitions';
 
 export default function Petitions() {
 
     const [profileImage, setProfileImage] = useState("/UserProfile/defaultProfile.svg");
   return (
     <div className='w-full lg:bg-[#FAFCFF]'>
-      <Topbar profileImage={profileImage} />
+      {/* <Topbar profileImage={profileImage} />
       <div className="flex w-full">
         <Sidebar />
         <SignedPetitions />
-      </div>
+      </div> */}
     </div>
   )
 }
