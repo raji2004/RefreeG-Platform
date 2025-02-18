3998b993a382a46d49b5731324026729701a1b7a
raji <kingraj1344@gmail.com>
2025-02-18 17:28:15 +0100

    refactor: comment out UserProfile component code for future revisions

--- src/app/UserFlow/UserProfile/page.tsx
+++ src/app/UserFlow/UserProfile/page.tsx
@@ -1,20 +1,20 @@
-"use client"
+// "use client"
 
-import React, { useState } from "react";
-import Sidebar from "@/components/Sidebar";
-import Topbar from "@/components/Topbar";
-import GeneralInfo from "@/components/UserProfileComponents/generalInfo";
+// import React, { useState } from "react";
+// import Sidebar from "@/components/Sidebar";
+// import Topbar from "@/components/Topbar";
+// import GeneralInfo from "@/components/UserProfileComponents/generalInfo";
 
-export default function UserProfile() {
-  const [profileImage, setProfileImage] = useState("/UserProfile/defaultProfile.svg");
+// export default function UserProfile() {
+//   const [profileImage, setProfileImage] = useState("/UserProfile/defaultProfile.svg");
 
-  return (
-    <div>
-      <Topbar profileImage={profileImage} />
-      <div className="flex w-full">
-        <Sidebar />
-        <GeneralInfo profileImage={profileImage} setProfileImage={setProfileImage} />
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Topbar profileImage={profileImage} />
+//       <div className="flex w-full">
+//         <Sidebar />
+//         <GeneralInfo profileImage={profileImage} setProfileImage={setProfileImage} />
+//       </div>
+//     </div>
+//   );
+// }
