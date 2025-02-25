5bbd3fdd923188a25534ceb4122e9e3c84bbca56
Oghenetega Gbiyede <tega2112@gmail.com>
2025-02-25 23:26:57 +0100

    Explore Causes
    Explore causes front end done.
    Pending backend database

---
+++ src/app/explorecauses/_components/foodRelief.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React from "react";
+import Image from "next/image";
+import { H2, P, Ol } from "@/components/typograpy";
+import { DonationCarousel } from "./exploredonationcarosuel";
+import { ChevronRight } from "lucide-react";
+
+export default function FoodRelief() {
+    return (
+        <div>
+            <div className="flex justify-between items-center mb-6">
+                <div>
+                    <H2>Food Relief</H2>
+                    <P>These causes contribute to the those in need of food</P>
+                </div>
+                <a href="#" className="text-blue-600 hover:underline flex items-center">View all <ChevronRight size={20} /></a>
+                
+            </div>
+            <DonationCarousel />
+        </div>
+       
+    );
+
+
+
+
+
+};
--- src/components/CauseCard.tsx
+++ src/components/CauseCard.tsx
@@ -106,7 +106,7 @@ export const MainCauseCard: React.FC<MainCauseCardProps> = ({
       </div>
 
       {/* Donate Button */}
-     {!hideButton && <div className="flex justify-center mt-4">
+     {!hideButton && <div className="flex justify-left mt-4">
         <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
           Donate now
         </button>
