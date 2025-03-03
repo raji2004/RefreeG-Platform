9f75fcc74c7b7df14df69066aa78a967f39dd905
Nomso <nomsounaogu@gmail.com>
2025-03-03 14:05:43 +0100

    fixed payment page error

--- src/app/(payment)/payment1/page.tsx
+++ src/app/(payment)/payment1/page.tsx
@@ -7,13 +7,13 @@ import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 
 // import BackgroundCard from "/donation_flow/cause_background.svg"
-import { Navbar, NavbarMedium, NavbarSmall } from "@/components/ui/navbar";
-import ClientHome from "@/components/ui/windowSize";
+// import { Navbar, NavbarMedium, NavbarSmall } from "@/components/ui/navbar";
+// import ClientHome from "@/components/ui/windowSize";
 
 export default function Page() {
   return (
     <div className="">
-      <ClientHome></ClientHome>
+      {/* <ClientHome></ClientHome> */}
       <div className="max-w-7xl p-3 space-y-3 mx-auto my-20 ">
         <Image
           className="w-full"
