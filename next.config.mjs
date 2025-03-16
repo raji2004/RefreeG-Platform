1af29e6e524bb4b4836802eea45a7f3a9bf2e4d6
Raji Muhammad Muzzamil <105373159+raji2004@users.noreply.github.com>
2025-03-16 09:35:46 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: 'https://refree-g-platform.vercel.app',
-    basePath: "/app",
+   
 };
 
 export default nextConfig;
