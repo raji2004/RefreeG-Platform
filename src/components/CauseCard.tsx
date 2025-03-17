d2eadbfa08087cb1dce2df8eb9c30bee31e3ba74
Nomso <nomsounaogu@gmail.com>
2025-03-17 11:56:01 +0100

    updated CauseCard

--- src/components/CauseCard.tsx
+++ src/components/CauseCard.tsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import DonationProgress from "../components/ui/donationProgress";
-import { Bookmark } from "lucide-react";
+import BookmarkButton from "./ui/BookmarkButton";
 import { MainCauseCardProps } from "@/lib/type";
-import Link from "next/link";
-
-
 
 export const MainCauseCard: React.FC<MainCauseCardProps> = ({
   img,
