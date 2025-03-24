4a92131004edb399ffcdc47c531130d84758dc0a
Raji Muhammad Muzzamil <105373159+raji2004@users.noreply.github.com>
2025-03-24 02:06:01 +0100

    Revert "Rs 185 fix the sign out functionality. when a user signs out he should be redirected to the home page"

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,56 +1,31 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const signUpSchema = z.object({
-  email: z.string().email("Invalid email format"),
-  password: z.string().min(8, "Password must be at least 8 characters long"),
-  confirmPassword: z
-    .string()
-    .min(8, "Password must be at least 8 characters long"),
-  firstName: z.string().min(2, "First name is required"),
-  lastName: z.string().min(2, "Last name is required"),
-  countryOfResidence: z.string().min(2, "Country of residence is required"),
-  DOB: z.date(),
-  phoneNumber: z
-    .string()
-    .min(11, "Phone number must be 11 digits")
-    .max(11, "Phone number must be 11 digits"),
-  bvn: z
-    .number()
-    .refine((value) => !isNaN(value), {
-      message: "BVN must be a valid number",
-    }),
-  nin: z
-    .number()
-    .refine((value) => !isNaN(value), {
-      message: "NIN must be a valid number",
-    }),
-  pin: z
-    .number()
-    .refine((value) => !isNaN(value), {
-      message: "NIN must be a valid number",
-    }),
-  donationPreference: z
-    .array(z.string())
-    .nonempty("Please select at least one donation preference"), // Updated to accept an array of strings
+    email: z.string().email("Invalid email format"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    confirmPassword: z.string().min(8, "Password must be at least 8 characters long"),
+    firstName: z.string().min(2, "First name is required"),
+    lastName: z.string().min(2, "Last name is required"),
+    countryOfResidence: z.string().min(2, "Country of residence is required"),
+    DOB: z.date(),
+    phoneNumber: z.string().min(11, "Phone number must be 11 digits").max(11, "Phone number must be 11 digits"),
+    bvn: z.number().refine(value => !isNaN(value), { message: "BVN must be a valid number" }),
+    nin: z.number().refine(value => !isNaN(value), { message: "NIN must be a valid number" }),
+    pin:z.number().refine(value => !isNaN(value), { message: "NIN must be a valid number" }),
+    donationPreference:z.string(),
 });
 
 export const compnanySchema = z.object({
-  organizatonName: z.string().min(2, "Organization name is required"),
-  organizationType: z.string().min(2, "Organization type is required"),
-  organizationLocation: z.string().min(2, "Organization location is required"),
-  taxIdentificationNumber: z
-    .string()
-    .min(2, "Invalid tax identification number"),
-  fullName: z.string().min(2, "Full name is required"),
-  email: z.string().email("Invalid email format"),
-  phoneNo: z
-    .number()
-    .min(11, "Phone number must be 11 digits")
-    .max(11, "Phone number must be 11 digits"),
-  positon: z.string().min(2, "Position is required"),
-});
-
+    organizatonName: z.string().min(2, "Organization name is required"),
+    organizationType: z.string().min(2, "Organization type is required"),
+    organizationLocation: z.string().min(2, "Organization location is required"),
+    taxIdentificationNumber: z.string().min(2,"invalid tax identification number"),
+    fullName: z.string().min(2, "Full name is required"),
+    email: z.string().email("Invalid email format"),
+    phoneNo: z.number().min(11, "Phone number must be 11 digits").max(11, "Phone number must be 11 digits"),
+    positon: z.string().min(2, "Position is required"),
+})
 export const loginSchema = signUpSchema.pick({
-  email: true,
-  password: true,
+    email: true,
+    password: true,
 });
