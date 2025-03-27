cec278968e23c9fcbd0ddcce1ffef2fcd7be0873
Nomso <nomsounaogu@gmail.com>
2025-03-27 10:31:10 +0100

    fixed webpack errors

--- src/app/(home)/OurStory/page.tsx
+++ src/app/(home)/OurStory/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Hero from "./_components/hero";
-import JoinCommunity from "../OurMission/_components/JoinCommunity";
 import Stories from "./_components/stories";
 
 export default function Impact() {
     return (
         <div className="w-full">
             <Hero />
-            <Stories />
-            <JoinCommunity />            
+            <Stories />   
         </div>
     )
 }
